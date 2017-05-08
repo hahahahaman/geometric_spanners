dad7844c882865a9c95161f4538b84a90645d145
Edward Ye <ye.edward.zhijia@gmail.com>
2017-05-08 17:54:23 -0400

    Looks like the algorithm has been implemented correctly.

--- algorithms.py
+++ algorithms.py
@@ -3,6 +3,8 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import math, random
 
+random.seed()
+
 # what is a geometric graph?
 # set of points in euclidean space
 # I will use networkx Graph object
@@ -90,22 +92,33 @@ def geometric_graph_stretch_factor(g, dimension=2):
     """
 
     pos = nx.get_node_attributes(g, 'pos')
-    dists = nx.floyd_warshall(g) # get minimum path between all vertices
+    num_nodes = g.number_of_nodes()
+    num_edges = g.number_of_edges()
 
-    # print(dists)
+    min_path_lengths = 0
+
+    # get minimum path between all vertices
+    # dijkstra for all vertices is O(|V||E|log|V|)
+    # floyd-warshall is O(|V|^3)
+    if(num_edges < (num_nodes ** 2)/ math.log2(num_nodes)):
+        print("dijkstra")
+        min_path_lengths = nx.all_pairs_dijkstra_path_length(g)
+    else:
+        print("floyd-warshall")
+        min_path_lengths = nx.floyd_warshall(g)
+
+    # print(min_path_lengths)
 
     max_stretch_factor = 0
 
     # print(dists[0][9])
     # print(euclidean_distance(pos[0], pos[9]))
 
-    num_nodes = g.number_of_nodes()
-
     # get stretch factor between all points
     # and find max stretch factor
     for i in range(num_nodes-1):
         for j in range(i+1, num_nodes):
-            stretch_factor = dists[i][j] / euclidean_distance(pos[i], pos[j])
+            stretch_factor = min_path_lengths[i][j] / euclidean_distance(pos[i], pos[j])
 
             # print(dists[i][j])
             # print(euclidean_distance(pos[i], pos[j]))
@@ -117,12 +130,37 @@ def geometric_graph_stretch_factor(g, dimension=2):
     return max_stretch_factor
 
 def random_points(n):
-    pnts = []
+    points = []
     for i in range(n):
-        pnts.append((random.random(),random.random()))
-    return pnts
+        points.append((random.random(),random.random()))
+    return points
 
-def matching(c1, c2):
+def random_convex_points(n):
+    points = []
+    totalx = 0
+    totaly = 0
+    for i in range(n-1):
+        points.append((random.uniform(-1.0, 1.0), random.uniform(-1.0, 1.0)))
+        totalx += points[i][0]
+        totaly += points[i][1]
+
+    points.append((-totalx, -totaly))
+    points.sort(key=lambda p: math.atan2(p[1],p[0]))
+
+    totalx = 0
+    totaly = 0
+    convex_points = []
+    for p in points:
+        totalx += p[0]
+        totaly += p[1]
+        convex_points.append((totalx, totaly))
+    return convex_points
+
+def plot_edges(s, color='k', linestyle='-'):
+    for i in range(len(s)-1):
+        plt.plot([s[i][0], s[i+1][0]], [s[i][1], s[i+1][1]], color=color, linestyle=linestyle)
+
+def matching(c1, c2, dists):
     """
     INPUT: Two linearly separated chains C1 and C2,
     such that the vertices of C1 U C2 are in convex position
@@ -131,9 +169,34 @@ def matching(c1, c2):
     of C1 and the points of C2
     """
 
-    if(c1 == [] or c2 == []):
-        return []
+    if(c1 == [] or c2 == []): return []
 
+    closest = (c1[0], c2[0])
+    indices = (0, 0)
+    for i in range(len(c1)):
+        a = c1[i]
+        for j in range(len(c2)):
+            b = c2[j]
+            # print(a, ":", b)
+            if dists[a][b] < dists[closest[0]][closest[1]]:
+                closest = (a,b)
+                indices = (i,j)
+
+    # print(len(c1), ":", indices[0])
+    # print(len(c2), ":", indices[1])
+
+    c1x = c1[:indices[0]]
+    c1y = c1[indices[0]+1:]
+
+    c2x = c2[:indices[1]]
+    c2y = c2[indices[1]+1:]
+    # print(c1x)
+    # print(c1y)
+    # print(c2x)
+    # print(c2y)
+    return [closest] + matching(c1x, c2y, dists) + matching(c1y, c2x, dists)
+
+'''
 def diametrical_pair_indices(s):
     num_points = len(s)
     if(num_points == 0):
@@ -144,7 +207,7 @@ def diametrical_pair_indices(s):
     max_dist = 0
 
     for i in range(num_points-1):
-        for j in range(i, num_points):
+        for j in range(i+1, num_points):
             dist =  euclidean_distance(s[i], s[j])
             if(dist > max_dist):
                 p = i
@@ -152,20 +215,41 @@ def diametrical_pair_indices(s):
                 max_dist = dist
 
     return (p,q)
+'''
 
-def deg3_plane_spanner(U, L):
+def deg3_plane_spanner(s):
+    U,L = hulls(s)
     graph = nx.Graph()
+
+    # Upper hull and lower hull share the first and last vertices in their list
+    # Lower hull is clockwise, must be reversed
     hull = U[1:] + list(reversed(L[:-1]))
 
-    # graph.add_nodes_from(hull)
+    point_to_index = {}
+    dists = {}
+    for i in range(len(hull)):
+        point_to_index[hull[i]] = i
+        dists[hull[i]] = {}
+
+    for i in range(len(hull)-1):
+        for j in range(i+1, len(hull)):
+            d = euclidean_distance(hull[i], hull[j])
+            dists[hull[i]][hull[j]] = d
+            dists[hull[j]][hull[i]] = d
+
     for i in range(len(hull)):
         graph.add_node(i, pos=hull[i])
 
+        j = (i+1) % len(hull)
+        graph.add_edge(i, j, weight=euclidean_distance(hull[i], hull[j]))
+
     dpair = diameter(U, L)
-    dpair_ind = []
-    for i in range(len(hull)):
-        if(hull[i] == dpair[0] or hull[i] == dpair[1]):
-            dpair_ind += [i]
+    dpair_index = (point_to_index[dpair[0]], point_to_index[dpair[1]])
+    # for i in range(len(hull)):
+    #     if(hull[i] == dpair[0] or hull[i] == dpair[1]):
+    #         dpair_ind.append(i)
+
+    # plot_edges(dpair, color='r', linestyle=':')
 
     # chains
     c1 = []
@@ -173,10 +257,10 @@ def deg3_plane_spanner(U, L):
 
     second = False
     for i in range(len(hull)-1):
-        current = (i+dpair_ind[0]+1)%len(hull)
+        current = (i+dpair_index[0]+1)%len(hull)
         p = hull[current]
 
-        if(current == dpair_ind[1]):
+        if(current == dpair_index[1]):
             second = True
             continue
 
@@ -185,18 +269,23 @@ def deg3_plane_spanner(U, L):
         else:
             c1.append(p)
 
+    matching_edges = matching(c1, c2, dists)
+
+    for e in matching_edges:
+        graph.add_edge(point_to_index[e[0]], point_to_index[e[1]],
+                       weight=dists[e[0]][e[1]])
+
     return graph
 
 # convex hull (monotone chain by x-coordinate) and diameter of a set of points
 # David Eppstein, UC Irvine, 7 Mar 2002
 
-
 def orientation(p,q,r):
-    '''Return positive if p-q-r are clockwise, neg if ccw, zero if colinear.'''
+    """Return positive if p-q-r are clockwise, neg if ccw, zero if colinear."""
     return (q[1]-p[1])*(r[0]-p[0]) - (q[0]-p[0])*(r[1]-p[1])
 
 def hulls(Points):
-    '''monotone chain to find upper and lower convex hulls of a set of 2d points.'''
+    """Monotone chain to find upper and lower convex hulls of a set of 2d points."""
     U = []
     L = []
     # Points = Points[np.lexsort((Points[:,1],Points[:,0]))]
@@ -209,9 +298,9 @@ def hulls(Points):
     return U,L
 
 def rotatingCalipers(U, L):
-    '''Given a list of 2d points, finds all ways of sandwiching the points
+    """Given a list of 2d points, finds all ways of sandwiching the points
 between two parallel lines that touch one point each, and yields the sequence
-of pairs of points touched by each pair of lines.'''
+of pairs of points touched by each pair of lines."""
     i = 0
     j = len(L) - 1
     while i < len(U) - 1 or j > 0:
@@ -229,44 +318,46 @@ of pairs of points touched by each pair of lines.'''
         else: j -= 1
 
 def diameter(U,L):
-    '''Given a list of 2d points, returns the pair that's farthest apart.'''
+    """Given a list of 2d points, returns the pair that"s farthest apart."""
     diam,pair = max([((p[0]-q[0])**2 + (p[1]-q[1])**2, (p,q))
                      for p,q in rotatingCalipers(U,L)])
     return pair
 
-points = random_points(100)
-U,L = hulls(points)
-hull = U[1:] + list(reversed(L[:-1]))
-dpair = diameter(U, L)
-dpair_ind = []
-for i in range(len(hull)):
-    if(hull[i] == dpair[0] or hull[i] == dpair[1]):
-        dpair_ind += [i]
 
-# chains
-c1 = []
-c2 = []
+# hull = U[1:] + list(reversed(L[:-1]))
+# dpair = diameter(U, L)
+# dpair_ind = []
+# for i in range(len(hull)):
+#     if(hull[i] == dpair[0] or hull[i] == dpair[1]):
+#         dpair_ind += [i]
 
-second = False
-for i in range(len(hull)-1):
-    current = (i+dpair_ind[0]+1)%len(hull)
-    p = hull[current]
+# # chains
+# c1 = []
+# c2 = []
 
-    if(current == dpair_ind[1]):
-        second = True
-        continue
+# second = False
+# for i in range(len(hull)-1):
+#     current = (i+dpair_ind[0]+1)%len(hull)
+#     p = hull[current]
 
-    if(second == True):
-        c2.append(p)
-    else:
-        c1.append(p)
+#     if(current == dpair_ind[1]):
+#         second = True
+#         continue
+
+#     if(second == True):
+#         c2.append(p)
+#     else:
+#         c1.append(p)
 
 
-G = random_weighted_geometric_graph(100, 0.2)
+# G = random_weighted_geometric_graph(100, 0.2)
 
-g = deg3_plane_spanner(U,L)
-pos = nx.get_node_attributes(g, 'pos')
-print(pos)
+points = random_convex_points(10)
+G = deg3_plane_spanner(points)
+pos = nx.get_node_attributes(G, 'pos')
+print("number of nodes:", G.number_of_nodes())
+print(geometric_graph_stretch_factor(G))
+# print(pos)
 
 # square
 # G = nx.Graph()
@@ -280,36 +371,38 @@ print(pos)
 # nx.set_node_attributes(G,'pos',{0:[0,0], 1:[0.3,0.3], 2:[0.5,0.5], 3:[0.299,0.3]})
 # G.add_weighted_edges_from([(0,1,0.5), (1,2,0.5), (2,3,0.5), (3,0,0.5)]) #wrong
 
-print(geometric_graph_stretch_factor(G))
+# print(geometric_graph_stretch_factor(G))
 
-pos = nx.get_node_attributes(G, 'pos')
+# pos = nx.get_node_attributes(G, 'pos')
 
 ### plotting
 
-# find point closest to the center(0.5,0.5)
-dmin = 1
-ncenter = 0
-for n in pos:
-    x,y = pos[n]
-    d=(x-0.5) ** 2 + (y-0.5) ** 2
-    if d < dmin:
-        ncenter = n
-        dmin = d
+# #find point closest to the center(0.5,0.5)
+# dmin = 1
+# ncenter = 0
+# for n in pos:
+#     x,y = pos[n]
+#     d=(x-0.5) ** 2 + (y-0.5) ** 2
+#     if d < dmin:
+#         ncenter = n
+#         dmin = d
 
-#color by path length from node near center
-p=nx.single_source_shortest_path_length(G,ncenter)
+# #color by path length from node near center
+# p=nx.single_source_shortest_path_length(G,ncenter)
 
 plt.figure(figsize=(8,8))
-nx.draw_networkx_edges(G,pos,nodelist=[ncenter], alpha=0.4)
-nx.draw_networkx_nodes(G,pos,
-                       nodelist=p.keys(),
-                       node_size=80,
-                       node_color=list(p.values()),
-                       cmap=plt.cm.Oranges_r)
+# nx.draw_networkx_edges(G,pos,nodelist=[ncenter], alpha=0.4)
+# nx.draw_networkx_nodes(G,pos,
+#                        nodelist=p.keys(),
+#                        node_size=80,
+#                        node_color=list(p.values()),
+#                        cmap=plt.cm.Oranges_r)
 
-plt.xlim(-0.05, 1.05)
-plt.ylim(-0.05, 1.05)
+nx.draw_networkx_edges(G, pos, alpha=0.4)
+nx.draw_networkx_nodes(G, pos, node_size=20)
+
+# plt.xlim(-0.05, 1.05)
+# plt.ylim(-0.05, 1.05)
 # plt.axis('off')
 # plt.savefig('random_geometric_graph.png')
 plt.show()
-
