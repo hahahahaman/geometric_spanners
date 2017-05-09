173b976224fa97469be1875a9e8c057c4c2c8046
Edward Ye <ye.edward.zhijia@gmail.com>
2017-05-09 19:23:10 -0400

    1- sqrt(3)/2

--- oval.py
+++ oval.py
@@ -1,5 +1,8 @@
 import math
 
+def factor(r):
+    return (1 - 3**(1/2)/2) * r
+
 # ramanujan approximation of perimeter of oval
 def approx(a, b):
     return math.pi * (3 * (a+b) - ((3*a + b) * (a + 3*b)) ** (1/2))
